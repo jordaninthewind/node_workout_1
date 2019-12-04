b32ed6e8437b88024528b73d3070ef2a5d9cf94c
Jordan Kline <jordan.kline@gmail.com>
2019-12-03 17:49:31 -0800

    completed task 1-2 and added files from review

--- task1-1.js
+++ task1-1.js
@@ -1,11 +1,10 @@
-process.argv.shift();
-process.argv.shift();
-const x = process.argv.join(" ");
-console.log(x   )
-let y = '';
+const x = process.argv.filter((arg, idx) => idx > 1).join(" ")
+let y = ''
 
+console.log(x)
 for (let i = x.length - 1; i >= 0; i--) {
     y += x[i]
 }
 
 console.log(y)
+return y
--- task1-2.js
+++ task1-2.js
@@ -1,1 +1,14 @@
-console.log('this is the future location of homework 1.2')
+const csv = require("csvtojson");
+const fs = require("fs");
+const filePath = "./csv/node_mentoring_t1_2_input_example.csv";
+const file = csv()
+  .fromFile(filePath)
+  .then(json => writeFile(json));
+
+function writeFile(file) {
+  const formattedFile = file.map(obj => JSON.stringify(obj));
+  fs.writeFile("formattedCsv.txt", formattedFile, err => {
+    if (err) throw err;
+    console.log("The file has been saved.");
+  });
+}
