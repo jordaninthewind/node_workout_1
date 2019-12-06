1b08cf7fc8d576965fed42ebdb41f13d3b7117ec
Jordan Kline <jordan.kline@gmail.com>
2019-12-06 09:06:05 -0800

    added babel config and dependencies

--- task1-3.js
+++ task1-3.js
@@ -1,1 +1,16 @@
-console.log('this is the future location of homework 1.3')
+import csvtojson from 'csvtojson';
+import fs from 'fs';
+
+const readFilePath = "./csv/node_mentoring_t1_2_input_example.csv";
+const writeFilePath = "./formattedCsv.txt";
+
+readWriteFile(readFilePath, writeFilePath);
+
+function readWriteFile(file, writeFile) {
+  const writeStream = fs.createWriteStream(writeFile)
+  fs.createReadStream(file)
+    .pipe(csvtojson())
+    .on('data', row => writeStream.write(row))
+    .on('error', error => console.log(error))
+    .on('end', () => console.log('File updated!'))
+}
