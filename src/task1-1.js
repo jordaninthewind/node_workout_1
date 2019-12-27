5cffd91c0aba5943f4e3cf5b7eedcb3df8ca39d2
Jordan Kline <jordan.kline@gmail.com>
2019-12-26 21:08:37 -0800

    update task1.1 to use process.stdin instead of readline

--- src/task1-1.js
+++ src/task1-1.js
@@ -1,22 +1,16 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-function reverse(str) {
-  return str
+const reverse = (str) =>
+  str
     .split('')
     .reverse()
     .join('');
-}
 
 function askQuestion() {
-  rl.question('Type input: \n', (input) => {
-    console.log(reverse(input));
-    askQuestion();
-  });
+  process.stdout.write('\nType a phrase to be reversed: ');
 }
 
 askQuestion();
+
+process.stdin.on('data', (data) => {
+  console.log(reverse(data.toString()));
+  askQuestion();
+});
